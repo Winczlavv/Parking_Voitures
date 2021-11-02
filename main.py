31be8c5c8228539fcab406993423485a86d2bca7
Paul Lurin <paullrn@MacBook-Pro-de-Paul.local>
2021-11-02 22:31:51 +0100

    deuxieme

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import random as rdm;
-
+from random import *
 class Parking:
 
     def __init__(self,nb_places):
-        self.P = []
         self.nb_places = nb_places
-        for i in range(nb_places):
-            self.P.append(rdm.randint(0,1))
+        self.P = [randint(0,1) for i in range(nb_places)]
         print(self.P)
     
     def se_garer(self,voiture):
@@ -19,7 +16,7 @@ class Parking:
                 if (j+1) % 2 == 1 and voiture.color == "bleu":
                     places_dispo += str(j+1) + ","
 
-        if voiture.color != "rouge" and voiture.color !="bleue" or places_dispo == places_dispo_init:
+        if voiture.color != "rouge" and voiture.color !="bleu" or places_dispo == places_dispo_init:
            places_dispo = "Vous ne pouvez pas vous garer. Passez votre chemin !."
         print(places_dispo.rstrip(places_dispo[-1]))
     
@@ -30,7 +27,6 @@ class Voiture:
 
 
 parking = Parking(10)
-voiture = Voiture("verte")
+voiture = Voiture("bleu")
 parking.se_garer(voiture)
-        
             
